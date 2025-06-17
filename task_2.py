4b49d0c9ff356d8408d4c001544921b28571e7ce
Vasyl <vasja112@gmail.com>
2025-06-17 17:31:02 +0300

    task_2 generator_numbers

--- task_2.py
+++ task_2.py
@@ -18,8 +18,9 @@ def sum_profit(text: str, func: Callable) -> float:
     return round(total_sum, 2)
 
 
-text = """Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід,
-доповнений додатковими надходженнями 27.45 і 324.00 доларів."""
+if __name__ == "__main__":
+    text = """Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід,
+    доповнений додатковими надходженнями 27.45 і 324.00 доларів."""
 
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}$")
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}$")
